9cca13c1bad824799c6f64522568470c1a8957c5
Jan Kropiwnicki <jan.kropiwnicki@infotech.edu.pl>
2023-07-20 13:03:52 +0000

    Finished Game of Life

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+var g = (x) => x.getAttribute('data-level');
+var s = (x, v) => x.setAttribute('data-level', v);
+var n = (X, Y) => {
+	var c = 0;
+	for(var x = X-1; x <= X+1; x++) {
+		for(var y = Y-1; y <= Y+1; y++) {
+		  	if(x !== X || y !== Y) {
+				try {c += parseInt(g(board[y][x])) / 4} catch {}
+		  	}
+		}
+	}
+	return c;
+}
 document.body.innerHTML += '<style>.ContributionCalendar-day{opacity: 1 !important}</style>';
 var startDate = Date.parse(
 	document.getElementsByClassName('ContributionCalendar-day')[0]
@@ -13,7 +26,24 @@ Array.from(document.getElementsByClassName('ContributionCalendar-day'))
 	.forEach((x) => {board[x[0] % 7][Math.floor(x[0] / 7)] = x[1]})
 board = board.map((x) => x.filter((el) => el));
 board.forEach((y) => y.forEach((el) => {
-	el.setAttribute('data-level', 0);
+	s(el, 0);
 	el.addEventListener('click', (ev) => 
-		el.setAttribute('data-level', el.getAttribute('data-level') == 0 ? 4 : 0))
+		s(el, g(el) == 0 ? 4 : 0))
 }));
+document.addEventListener('keypress', (ev) => {
+	if (ev.key === 'Enter') {
+		setInterval(() => {
+			var newBoard = board.map((y) => y.map((x) => g(x)));
+			for (var y = 0; y < board.length; y++) {
+				for (var x = 0; x < board[y].length; x++) {
+					newBoard[y][x] = 4 * (g(board[y][x]) == 4 ? 1 < n(x, y) && n(x, y) < 4 : n(x, y) == 3);
+				}
+			}
+			for (var y = 0; y < board.length; y++) {
+				for (var x = 0; x < board[y].length; x++) {
+					s(board[y][x], newBoard[y][x])
+				}
+			}
+		}, 300);
+	}
+});
